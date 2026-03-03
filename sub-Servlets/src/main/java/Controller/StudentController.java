926aa671428ed3110325f5e759155fe877c0d81f
Igihozo Belise <igihozobelise6@gmail.com>
2026-03-03 23:39:58 +0200

    new lesson

---
+++ sub-Servlets/src/main/java/Controller/StudentController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+//(req, res)
+@WebServlet("/me")
+public class StudentController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req);
+    }
+}
